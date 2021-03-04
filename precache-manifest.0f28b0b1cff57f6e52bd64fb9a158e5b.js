1a90158cea9f896ef34889aca7dac96f246deb75
OriyaKeshet-git <keshetoriya@gmail.com>
2021-03-04 18:56:08 +0200

    Updates

---
+++ precache-manifest.0f28b0b1cff57f6e52bd64fb9a158e5b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d750bb25bb7fdf330f2e",
+    "url": "/React-App--Movies/static/css/main.0833aa0b.chunk.css"
+  },
+  {
+    "revision": "d750bb25bb7fdf330f2e",
+    "url": "/React-App--Movies/static/js/main.d750bb25.chunk.js"
+  },
+  {
+    "revision": "9c8bbdad10e6575c302b",
+    "url": "/React-App--Movies/static/css/1.0862542a.chunk.css"
+  },
+  {
+    "revision": "9c8bbdad10e6575c302b",
+    "url": "/React-App--Movies/static/js/1.9c8bbdad.chunk.js"
+  },
+  {
+    "revision": "302c3a5b2ad9270ff500",
+    "url": "/React-App--Movies/static/js/runtime~main.302c3a5b.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-App--Movies/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-App--Movies/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-App--Movies/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-App--Movies/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-App--Movies/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "33f06f1132329f2d1f93302176964271",
+    "url": "/React-App--Movies/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/React-App--Movies/precache-manifest.0f28b0b1cff57f6e52bd64fb9a158e5b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/React-App--Movies/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
